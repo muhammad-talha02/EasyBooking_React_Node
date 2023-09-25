d51bf15bae1c35f3aa6a3bd0d3bf303690104676
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-09-25 06:02:51 +0000

    Before Buton component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Navbar } from  "../components"
 
 const Home = () => {
   return (
-    <div className="text-3xl font-bold underline text-red-600">
+    <div className="">
       <Navbar/>
       Home
     </div>
