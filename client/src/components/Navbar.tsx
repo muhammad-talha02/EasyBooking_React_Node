9eba2dc61448a6401c88d6acbe0b66b0790f0516
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-09-25 07:54:27 +0000

    Header Section designed

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,10 +6,10 @@ const Navbar = () => {
       <div className="navContainer w-[100%] max-w-[1024px] flex justify-between items-center text-[white]">
         <span className="logo font-bold">EasyBooking</span>
         <div className="navItems">
-          <button className="navButton ml-[20px] py-[5px] px-[10px] text-[--theme] bg-[white] rounded-sm">
+          <Button>
             Login
-          </button>
-          <Button classes="" action={() => alert("Hello ")}>
+          </Button>
+          <Button classes="ml-[20px]">
             Login
           </Button>
           <button className="navButton ml-[20px] ">Register</button>
@@ -19,9 +19,4 @@ const Navbar = () => {
   );
 };
 
-function test(x: string | undefined) {
-  let y;
-  y = 12;
-  alert("Hello " + x);
-}
 export default Navbar;
