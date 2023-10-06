3199f75fba543a09926d03e20351a8dd27a3b3c7
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-10-06 07:01:40 +0000

    Fixed -> issues in  header Component

--- client/src/sections/Header.tsx
+++ client/src/sections/Header.tsx
@@ -3,16 +3,19 @@ import { HeaderIcons } from "../utils/HeaderIcons";
 import { Button } from "../TailwindComponents";
 import { SearchBar } from "../components";
 
-
 type HeaderProps = {
-  type?:string
-}
-const Header = ({type}:HeaderProps) => {
+  mode?: string;
+};
+const Header = ({ mode }: HeaderProps) => {
   // const test = faBed
 
   return (
     <div className="header flex justify-center text-white bg-[--theme] relative">
-      <div className="headerContainer w-[100%] max-w-[1024px] mt-[20px] mx-[0px] mb-[100px]">
+      <div
+        className={`headerContainer w-[100%] max-w-[1024px] mt-[20px] mx-[0px] ${
+          mode === "list" ? "" : "mb-[100px]"
+        }`}
+      >
         <div className="headerList  flex gap-[40px] mb-8">
           {HeaderIcons?.map((item) => {
             return (
@@ -26,22 +29,23 @@ const Header = ({type}:HeaderProps) => {
             );
           })}
         </div>
-        {type !== "list" && <>
-          <h1 className="headerTitle text-3xl font-bold">
-          Discount for lifetime. It Genious!
-        </h1>
-        <p className="headerDesc my-[20px]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam dicta
-          explicabo deserunt quod maiores enim aliquid neque soluta sint, ea
-          consequuntur officia, mollitia adipisci architecto voluptatum nemo.
-          Facere, labore molestiae.
-        </p>
-        <Button classes="p-[10px] bg-[#0071c2] text-[white] border-0 font-medium">
-          Login / Register
-        </Button>
-        </>}
-        <SearchBar/>
-
+        {mode !== "list" && (
+          <>
+            <h1 className="headerTitle text-3xl font-bold">
+              Discount for lifetime. It Genious!
+            </h1>
+            <p className="headerDesc my-[20px]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+              dicta explicabo deserunt quod maiores enim aliquid neque soluta
+              sint, ea consequuntur officia, mollitia adipisci architecto
+              voluptatum nemo. Facere, labore molestiae.
+            </p>
+            <Button classes="p-[10px] bg-[#0071c2] text-[white] border-0 font-medium">
+              Login / Register
+            </Button>
+            <SearchBar />
+          </>
+        )}
       </div>
     </div>
   );
