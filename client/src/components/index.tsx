08993c202ab90e884aaceebf7ea82118a461643b
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-10-13 07:22:51 +0000

    Added -> Property Types on Home

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -3,3 +3,5 @@ export { default as SearchBar } from "./SearchBar";
 export { default as OptionsCard } from "./OptionsCard";
 export { default as FeaturedList } from "./FeaturedList";
 export { default as FeatureItem } from "./FeatureItem";
+export { default as PropertyTypesList } from "./PropertyTypesList";
+export { default as ProperttyTypeItem } from "./ProperttyTypeItem";
