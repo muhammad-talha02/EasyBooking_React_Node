c917483e8584ac4c5d6bd93c00a46461183cc957
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-10-20 06:59:28 +0000

    Update -> gallery Modal

--- client/src/components/GalleryModal.tsx
+++ client/src/components/GalleryModal.tsx
@@ -1,25 +1,28 @@
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ReactDOM from "react-dom";
-import { H2 } from "../TailwindComponents/Typorgraphy/Headings";
+import { ReactNode } from "react";
 type ModalProps = {
-  title?: string;
   show: boolean;
   closeModal: () => void;
+  children?:ReactNode
 };
 
-const Modal = ({ show, closeModal, title }: ModalProps) => {
+const GalleryModal = ({ show, closeModal, children}: ModalProps) => {
   const portal: HTMLElement | Element = document.getElementById("portal")!;
   if (!show) return null;
   return ReactDOM.createPortal(
     <div className="Overlay backdrop-blur-[2px] w-[100%] fixed overflow-y-hidden top-0 left-0 h-[100vh] bottom-0 z-50 bg-[#00000069] flex justify-center items-center">
-      <div className="Modal mx-4  w-full md:w-[80%] max-w-[1024px] bg-white h-[50%] rounded-md">
-        <div className="modal-content">
-          <div className="modalHeader flex justify-between">
-            <H2>{title}</H2>
-            <button onClick={closeModal} className="text-[22px] mr-3">
-              <FontAwesomeIcon icon={faXmark} />
+      <div className="Modal mx-4  w-full rounded-md">
+        <div className="modal-content h-full flex flex-col">
+          <div className="modalHeader">
+            {/* <H2>{title}</H2> */}
+            <button onClick={closeModal} className="text-[22px] mr-3 float-right mb-3 rounded-full ">
+              <FontAwesomeIcon className="text-white" icon={faXmark} />
             </button>
+          </div>
+          <div className="modalBody h-[90%]">
+            {children}
           </div>
         </div>
       </div>
@@ -28,4 +31,4 @@ const Modal = ({ show, closeModal, title }: ModalProps) => {
   );
 };
 
-export default Modal;
+export default GalleryModal;
