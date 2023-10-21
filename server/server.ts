af506b9dba77b57365ff06ae48286bb2957c9f31
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-10-21 07:15:12 +0000

    Setup -> MongoDB Connection

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,20 @@
-import { configDotenv } from "dotenv";
-configDotenv()
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import { app } from "./app";
+dotenv.config();
 
-app.listen(process.env.PORT , ()=>{
-    console.log(`App is Running at ${process.env.PORT}`)
-})
+const connectDB = () => {
+  mongoose
+    .connect(process.env.DB_URL as string)
+    .then(() => console.log("Database Connected"))
+    .catch(() => {
+      console.log("Error In Database Connection");
+    });
+};
+
+mongoose.connection.on("connected", ()=>console.log("connection stable"))
+
+app.listen(process.env.PORT, () => {
+  connectDB();
+  console.log(`App is Running at ${process.env.PORT}`);
+});
