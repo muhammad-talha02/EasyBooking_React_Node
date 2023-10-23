33a31228f8c699f6eff87dfb36d21f2a0fdcaa6e
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-10-23 07:06:48 +0000

    Feature -> Error Middleware

--- server/routes/hotelsRoute.ts
+++ server/routes/hotelsRoute.ts
@@ -1,10 +1,21 @@
 import express, { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../utils/ErrorHandler";
 
 const hotelsRouter = express.Router();
 
-hotelsRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
+// Create
 
-    res.send("Hotels")
+hotelsRouter.post(
+  "/hotel",
+  (req: Request, res: Response, next: NextFunction) => {
+    res.send("Hotels");
+  }
+);
+
+
+
+hotelsRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hotels");
 });
 
-export default hotelsRouter
+export default hotelsRouter;
