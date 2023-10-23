f9b6761be21f91f6072c342b23f2498e03626fa0
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-10-23 07:06:48 +0000

    Feature -> Error Middleware

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,8 @@
 import express, { NextFunction, Request, Response } from "express";
 import userRouter from "./routes/userRoute";
 import hotelsRouter from "./routes/hotelsRoute";
+import ErrorMiddleware from "./Middlewares/ErrorMiddleware";
+import ErrorHandler from "./utils/ErrorHandler";
 
 export const app = express();
 
@@ -8,13 +10,20 @@ export const app = express();
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response, next:NextFunction) => {
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send({
-    success:true,
-    message:"Serevr is Working"
+    success: true,
+    message: "Serevr is Working",
   });
 });
 
+app.use("/api/users", userRouter);
+app.use("/api/hotels", hotelsRouter);
 
-app.use("/api/users", userRouter)
-app.use("/api/hotels", hotelsRouter)
+app.all("*", (req, res, next) => {
+  next(new ErrorHandler("Route not found", 400));
+});
+
+// Error Middleware
+
+app.use(ErrorMiddleware);
