22e5ffcccf2ba87d13de0951e9543e2ee214c1ab
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-11-10 13:34:18 +0000

    Added -> Routes for Hotels

--- server/routes/hotelsRoute.ts
+++ server/routes/hotelsRoute.ts
@@ -1,21 +1,15 @@
 import express, { NextFunction, Request, Response } from "express";
-import ErrorHandler from "../utils/ErrorHandler";
+// import ErrorHandler from "../utils/ErrorHandler";
+import { createHotel } from "../controllers/hotelsController";
 
 const hotelsRouter = express.Router();
 
 // Create
 
-hotelsRouter.post(
-  "/hotel",
-  (req: Request, res: Response, next: NextFunction) => {
-    res.send("Hotels");
-  }
-);
+hotelsRouter.post("/hotel", createHotel);
 
-
-
-hotelsRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hotels");
-});
+// hotelsRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
+//   res.send("Hotels");
+// });
 
 export default hotelsRouter;
