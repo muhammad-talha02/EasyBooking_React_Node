48c5b909d9c6e4cf82129d5fb11051cbcdf962a8
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-11-11 07:29:04 +0000

    Added -> Hotels CRUD Done !

--- server/routes/hotelsRoute.ts
+++ server/routes/hotelsRoute.ts
@@ -1,12 +1,16 @@
 import express, { NextFunction, Request, Response } from "express";
 // import ErrorHandler from "../utils/ErrorHandler";
-import { createHotel } from "../controllers/hotelsController";
+import { createHotel, deleteHotel, getAllHotels, getSingleHotel, updateHotel } from "../controllers/hotelsController";
 
 const hotelsRouter = express.Router();
 
 // Create
 
 hotelsRouter.post("/hotel", createHotel);
+hotelsRouter.put("/hotel/:id", updateHotel);
+hotelsRouter.delete("/hotel/:id", deleteHotel);
+hotelsRouter.get("/hotel/:id", getSingleHotel);
+hotelsRouter.get("/", getAllHotels);
 
 // hotelsRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
 //   res.send("Hotels");
