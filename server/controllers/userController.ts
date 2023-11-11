f51afcf3e5aeb77d14428823f7dc22983537abb8
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-11-11 11:13:15 +0000

    Added -> Login Controller

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -2,20 +2,48 @@ import express, { NextFunction, Request, Response } from "express";
 import { catchAsyncError } from "../Middlewares/catchAyncError";
 import User from "../models/user.model";
 import ErrorHandler from "../utils/ErrorHandler";
-
+import bcrypt from "bcryptjs";
+// Register
 export const registerUser = catchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     console.log(req.body);
     try {
       const newUser = new User({
         username: req.body.username,
-        email: req.body.username,
-        password: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
       });
       const savedUser = await newUser.save();
       res.status(200).json(savedUser);
-    } catch (error:any) {
-        return next(new ErrorHandler(error.message , 404))
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 404));
+    }
+  }
+);
+
+// Login
+
+export const loginUser = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        return next(new ErrorHandler("Please Enter email and password", 400));
+      }
+        // const user = await User.findOne({ email });
+      const user = await User.findOne({ email });
+      console.log("User",user);
+      if (!user) {
+        return next(new ErrorHandler("Email not exists", 400));
+      }
+      const isPasswordMatch = await user.comparePassword(password);
+      if (!isPasswordMatch) {
+        return next(new ErrorHandler("Password INcorrect", 400));
+      }
+      res.status(200).json(user);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 404));
     }
   }
 );
