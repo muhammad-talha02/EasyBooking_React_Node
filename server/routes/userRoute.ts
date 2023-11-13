8edbfb7d3720232779610a648aa51929e679d984
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-11-13 13:56:51 +0000

    Added -> Update USAsr with verify token

--- server/routes/userRoute.ts
+++ server/routes/userRoute.ts
@@ -1,11 +1,17 @@
 import express from "express";
-import { getUser, loginUser, registerUser, updateUser } from "../controllers/userController";
+import {
+  getUser,
+  loginUser,
+  registerUser,
+  updateUser,
+} from "../controllers/userController";
+import { verifyToken, verifyUser } from "../utils/verifyToken";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
-userRouter.post("/update/:id", updateUser);
+userRouter.post("/update/:id", verifyUser, updateUser);
 userRouter.get("/:id", getUser);
 // userRouter.post("/", updateUser);
 
---
+++ server/utils/verifyToken.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Request, Response } from "express";
+import { catchAsyncError } from "../Middlewares/catchAyncError";
+import ErrorHandler from "./ErrorHandler";
+import jwt from "jsonwebtoken";
+
+interface verifyTokenRequest extends Request {
+  user: any;
+}
+export const verifyToken = catchAsyncError(
+  async (req: verifyTokenRequest, res: Response, next: NextFunction) => {
+    const token = req.cookies.access_token;
+
+    try {
+      if (!token) {
+        return next(new ErrorHandler("Session Expired", 401));
+      }
+      // console.log({ VerifyToken: token });
+
+      const getUser: any = jwt.verify(token, process.env.SECRET_KEY as string);
+      if (!getUser) {
+        return next(new ErrorHandler("User is not autneticated", 403));
+      }
+
+      req.user = getUser.user;
+      console.log("Token user", getUser);
+      next();
+    } catch (error) {
+      return next(new ErrorHandler("JWT EROR>>>>", 400));
+    }
+  }
+);
+
+export const verifyUser = catchAsyncError(
+  async (req: verifyTokenRequest, res: Response, next: NextFunction) => {
+    verifyToken(req, res, () => {
+      console.log("Verifying user", req.user.id);
+      if(req.user.id === req.params.id){
+        next()
+      }
+      else{
+        return next(new ErrorHandler("User not Valid", 400))
+      }
+    });
+  }
+);
