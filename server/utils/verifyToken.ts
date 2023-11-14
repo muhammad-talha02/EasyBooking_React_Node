b63d62c971b1fd1f7217d79b53752211a6bf21c7
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-11-14 08:26:30 +0000

    Added -> Authentication DOne

--- server/utils/verifyToken.ts
+++ server/utils/verifyToken.ts
@@ -1,59 +1,93 @@
-import { NextFunction, Request, Response } from "express";
-import { catchAsyncError } from "../Middlewares/catchAyncError";
-import ErrorHandler from "./ErrorHandler";
-import jwt from "jsonwebtoken";
+// import { NextFunction, Request, Response } from "express";
+// import { catchAsyncError } from "../Middlewares/catchAyncError";
+// import ErrorHandler from "./ErrorHandler";
+// import jwt from "jsonwebtoken";
 
-interface verifyTokenRequest extends Request {
-  user: any;
-}
-export const verifyToken = catchAsyncError(
-  async (req: verifyTokenRequest, res: Response, next: NextFunction) => {
-    const token = req.cookies.access_token;
+// interface verifyTokenRequest extends Request {
+//   user: any;
+// }
+// export const verifyUser = catchAsyncError(
+//   async (req: verifyTokenRequest, res: Response, next: NextFunction) => {
+//     const token = req.cookies.access_token;
 
-    try {
-      if (!token) {
-        return next(new ErrorHandler("Session Expired", 401));
-      }
-      // console.log({ VerifyToken: token });
+//     if (!token) {
+//       console.log("Token user", token);
+//       return next(new ErrorHandler("Session Expired", 401));
+//     }
+//     try {
+//       console.log({ VerifyToken: token });
 
-      const getUser: any = jwt.verify(token, process.env.SECRET_KEY as string);
-      if (!getUser) {
-        return next(new ErrorHandler("User is not autneticated", 403));
-      }
+//       const getUser: any = jwt.verify(token, process.env.SECRET_KEY as string);
+//       if (!getUser) {
+//         return next(new ErrorHandler("Token is not Valid", 403));
+//       }
 
-      req.user = getUser.user;
-      console.log("Token user", getUser);
-      next();
-    } catch (error) {
-      return next(new ErrorHandler("JWT EROR>>>>", 400));
-    }
-  }
-);
+//       req.user = getUser.user;
+//       if (req.user.id === req.params.id || req.user.isAdmin) {
+//         next();
+//       } else {
+//         return next(new ErrorHandler("User not Valid", 400));
+//       }
+//       // next();
+//     } catch (error) {
+//       return next(new ErrorHandler("JWT EROR>>>>", 400));
+//     }
+//   }
+// );
 
-export const verifyUser = catchAsyncError(
-  async (req: verifyTokenRequest, res: Response, next: NextFunction) => {
-    verifyToken(req, res, () => {
-      console.log("Verifying user", req.user.id);
-      if(req.user.id === req.params.id || req.user.isAdmin){
-        next()
-      }
-      else{
-        return next(new ErrorHandler("User not Valid", 400))
-      }
-    });
-  }
-);
 
-export const verifyAdmin = catchAsyncError(
-  async (req: verifyTokenRequest, res: Response, next: NextFunction) => {
-    verifyToken(req, res, () => {
-      // console.log("Verifying user", req.user.id);
-      if(req.user.isAdmin){
-        next()
-      }
-      else{
-        return next(new ErrorHandler("Hey you are not admin ok!", 400))
-      }
-    });
-  }
-);
+// export const verifyAdmin = catchAsyncError(
+//   async (req: verifyTokenRequest, res: Response, next: NextFunction) => {
+//     const token = req.cookies.access_token;
+
+//     if (!token) {
+//       console.log("Token user", token);
+//       return next(new ErrorHandler("Session Expired", 401));
+//     }
+//     try {
+//       console.log({ VerifyToken: token });
+
+//       const getUser: any = jwt.verify(token, process.env.SECRET_KEY as string);
+//       if (!getUser) {
+//         return next(new ErrorHandler("Token is not Valid", 403));
+//       }
+
+//       req.user = getUser.user;
+//       if (req.user.isAdmin) {
+//         next();
+//       } else {
+//         return next(new ErrorHandler("Hey you are not admin ok!", 400));
+//       }
+//       // next();
+//     } catch (error) {
+//       return next(new ErrorHandler("JWT EROR>>>>", 400));
+//     }
+//   }
+// );
+
+// export const verifyUser = catchAsyncError(
+//   async (req: verifyTokenRequest, res: Response, next: NextFunction) => {
+//     verifyToken(req, res, () => {
+//       console.log("Verifying user", req.user.id);
+//       if(req.user.id === req.params.id || req.user.isAdmin){
+//         next()
+//       }
+//       else{
+//         return next(new ErrorHandler("User not Valid", 400))
+//       }
+//     });
+//   }
+// );
+
+// export const verifyAdmin = catchAsyncError(
+//   async (req: verifyTokenRequest, res: Response, next: NextFunction) => {
+//     // verifyToken(req, res, () => {
+//     console.log("Verifying user", req.user);
+//     if (req.user.isAdmin) {
+//       next();
+//     } else {
+//       return next(new ErrorHandler("Hey you are not admin ok!", 400));
+//     }
+//     // });
+//   }
+// );
