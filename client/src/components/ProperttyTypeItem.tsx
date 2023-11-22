560cfe250935e847f014747f9d10cf46e87a4697
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-11-22 12:00:08 +0000

    Feature -> Types of properties

--- client/src/components/ProperttyTypeItem.tsx
+++ client/src/components/ProperttyTypeItem.tsx
@@ -1,16 +1,25 @@
 import { H3, H5 } from "../TailwindComponents/Typorgraphy/Headings";
+interface IPropertyItem {
+  item: {
+    type: string;
+    count: number;
+  },
+  img:string
+}
 
-const ProperttyTypeItem = () => {
+const ProperttyTypeItem = ({ item, img }: IPropertyItem) => {
   return (
-    <div className="pListItem rounded-xl overflow-hidden cursor-pointer flex-1">
+    <div className="pListItem rounded-md overflow-hidden cursor-pointer flex-1">
       <img
         className="w-full h-[150px] object-cover"
-        src="https://images.unsplash.com/photo-1511091734515-e50d46c37240?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1587&q=80"
+        src={img}
         alt=""
       />
       <div className="pListTitles">
-        <H3 classes="font-medium">Hotels</H3>
-        <H5 classes="font-light">123 Hotels</H5>
+        <H3 classes="font-medium capitalize">{item.type}</H3>
+        <H5 classes="font-light capitalize">
+          {item.count} {item.type}
+        </H5>
       </div>
     </div>
   );
