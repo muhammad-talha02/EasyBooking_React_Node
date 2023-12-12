28e25eed0e8dc70af9bd6b4ced0a2562e273945a
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-12-12 13:16:23 +0000

    Added -> feature

--- client/src/components/FeaturedProperties.tsx
+++ client/src/components/FeaturedProperties.tsx
@@ -1,18 +1,23 @@
-import { PropertyCard } from "."
-import { H3 } from "../TailwindComponents/Typorgraphy/Headings"
-import { HomeContainer } from "./HOC"
+import { PropertyCard } from ".";
+import { H3 } from "../TailwindComponents/Typorgraphy/Headings";
+import useFetch from "../hooks/useFetch";
+import { HomeContainer } from "./HOC";
 
 const FeaturedProperties = () => {
+  const { data, loading } = useFetch("/api/hotels?featured=true&limit=3");
+  console.log("Feature ", data);
   return (
     <div className="w-full">
       <H3 classes="">Home Guests Love</H3>
       <div className="mt-[20px] flex gap-[20px] justify-between">
-    <PropertyCard/>
-    <PropertyCard/>
-    <PropertyCard/>
+        {
+          data?.map((item:any)=>{
+            return <PropertyCard  item={item} key={item._id}/>
+          })
+        }
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeContainer(FeaturedProperties)
+export default HomeContainer(FeaturedProperties);
