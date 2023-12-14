f88931417cb13c5a548d24a1131f0df80fa563df
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-12-14 13:56:55 +0000

    Featyre ->Search resilts

--- client/src/pages/List.tsx
+++ client/src/pages/List.tsx
@@ -1,27 +1,34 @@
-import { Navbar, SearchCard, SidebarSearch } from '../components'
-import Header from '../sections/Header'
+import { useState } from "react";
+import { H4 } from "../TailwindComponents/Typorgraphy/Headings";
+import { Navbar, SearchCard, SidebarSearch } from "../components";
+import Loader from "../components/Loader";
+import Header from "../sections/Header";
 
 const List = () => {
+  const [hotelData, setHotelData] = useState<any>({});
+  console.log("data", hotelData);
   return (
     <>
-      <Navbar/>
-      <Header mode="list"/>
+      <Navbar />
+      <Header mode="list" />
       <div className="lisContainer flex justify-center mt-10">
         <div className="lisWrapper w-full max-w-[1024px] gap-[20px] flex px-2">
-          <SidebarSearch/>
+          <SidebarSearch setHotelData={setHotelData} />
           <div className="listResults flex-[3]">
-            <SearchCard/>
-            <SearchCard/>
-            <SearchCard/>
-            <SearchCard/>
-            <SearchCard/>
-            <SearchCard/>
-            <SearchCard/>
+            {!hotelData?.isSuccess ? (
+              <Loader />
+            ) : hotelData?.data?.length > 0 ? (
+              hotelData?.data?.map((item: any) => (
+                <SearchCard key={item._id} item={item} />
+              ))
+            ) : (
+              <H4 classes="text-center">Not Found.</H4>
+            )}
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default List
+export default List;
