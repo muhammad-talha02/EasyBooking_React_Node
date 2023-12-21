b39b53d371babdd3e104dfd44f06cbf8327db0ab
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-12-21 13:53:32 +0000

    Added -> Login page i sdesigned

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import List from "./pages/List"
-import Hotel from "./pages/Hotel"
+import { Home, Hotel, List, Login } from "./pages"
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/hotels" element={<List/>}/>
-        <Route path="/hotel/:id" element={<Hotel/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotel/:id" element={<Hotel />} />
       </Routes>
     </BrowserRouter>
   )
