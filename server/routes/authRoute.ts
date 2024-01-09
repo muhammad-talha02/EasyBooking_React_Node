89c570c09e30565322c762c448bcd7084ab641bc
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2024-01-09 07:36:20 +0000

    Fixed -> issues for deployment in NOde TS

--- server/routes/authRoute.ts
+++ server/routes/authRoute.ts
@@ -1,9 +1,9 @@
-import express from "express"
-import { authencticateuser } from "../controllers/authController"
+// import express from "express"
+// import { authencticateuser } from "../controllers/authController"
 
-const authRouter = express.Router()
+// const authRouter = express.Router()
 
 
-authRouter.get("/", authencticateuser)
+// authRouter.get("/", authencticateuser)
 
-export default authRouter
+// export default authRouter
